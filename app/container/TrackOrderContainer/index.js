43b69b150c1d4ee5766301a5059e761dbe3ad78f
yagnesh <yagneshreshamwala@gmail.com>
2024-08-27 21:29:29 +0530

    applied changes

--- app/container/TrackOrderContainer/index.js
+++ app/container/TrackOrderContainer/index.js
@@ -1,23 +1,30 @@
-import React,{useRef,useEffect} from 'react';
-import MapView, { Marker } from 'react-native-maps';
-import useMqtt from '../../res/useMqtt';
+import React, { useRef, useEffect,useState } from "react";
+import MapView, { Marker } from "react-native-maps";
+import useMqtt from "../../res/useMqtt";
 import { useSelector } from "react-redux";
-import { images } from '../../res/images';
-import { Image } from 'react-native';
+import { images } from "../../res/images";
+import { Image } from "react-native";
 
 const TrackOrderContainer = () => {
-    const mapRef = useRef(null);
-    const appReducer = useSelector(state => state.appReducer);
+  const mapRef = useRef(null);
+  const appReducer = useSelector((state) => state.appReducer);
   const config = appReducer?.driverLocations;
   const location = useMqtt(config);
+  const [markerPosition, setMarkerPosition] = useState({
+    latitude: 37.78825,  // Default initial position
+    longitude: -122.4324,
+  });
 
   useEffect(() => {
-    if (location.lat && location.lng && mapRef.current) {
+    if (location.lat && location.lng) {
+      setMarkerPosition({
+        latitude: location.lat,
+        longitude: location.lng,
+      });
+
       mapRef.current.animateToRegion({
         latitude: location.lat,
         longitude: location.lng,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
       });
     }
   }, [location]);
@@ -27,25 +34,22 @@ const TrackOrderContainer = () => {
       ref={mapRef}
       style={{ flex: 1 }}
       initialRegion={{
-        latitude: location?.lat || 37.78825,
-        longitude: location?.lng || -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+       latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.01,
+        longitudeDelta:0.01,
       }}
     >
-      {location?.lat && location?.lng && (
         <Marker
-        coordinate={{ latitude: location.lat, longitude: location.lng }}
-        title="Driver Location"
-        description={`Lat: ${location.lat}, Lng: ${location.lng}`}
-      >
-        <Image
+          coordinate={markerPosition}
+          title="Driver Location"
+        >
+          <Image
             source={images.location}
-            style={{ width: 40, height: 40 }}  // Set width and height here
-            resizeMode="contain"  // Optional: Adjust how the image scales
+            style={{ width: 40, height: 40 }} // Set width and height here
+            resizeMode="contain" // Optional: Adjust how the image scales
           />
-      </Marker>
-      )}
+        </Marker>
     </MapView>
   );
 };
--- app/res/useMqtt.js
+++ app/res/useMqtt.js
@@ -13,52 +13,51 @@ init({
 });
 
 const useMqtt = (config) => {
-  const [location, setLocation] = useState({ lat: null, lng: null });
+  const [location, setLocation] = useState({ lat: 0.00001, lng: 0.00001 });
 
   useEffect(() => {
-          const setupMqtt = async () => {
-        try {
-          const client = new Paho.MQTT.Client(
-            `wss://${config?.host}/mqtt`,
-            config?.clientId
-          );
+    const setupMqtt = async () => {
+      try {
+        const options = {
+          useSSL: true,
+          userName: config?.userName,
+          password: config?.password,
+          onSuccess: () => {
+            // console.log("Connected to MQTT broker");
+            client.subscribe(config?.topic, { qos: config?.qos });
+          },
+          onFailure: (error) => {
+            // console.log("Connection failed:", error.errorMessage);
+            setupMqtt();
+          },
+        };
 
-          const options = {
-            useSSL: true,
-            userName: config?.userName,
-            password: config?.password,
-            onSuccess: () => {
-              console.log("Connected to MQTT broker");
-              client.subscribe(config?.topic, { qos: config?.qos });
-            },
-            onFailure: (error) => {
-              console.log("Connection failed:", error.errorMessage);
-              setupMqtt()
-            },
-          };
+        const client = new Paho.MQTT.Client(
+          `wss://${config?.host}/mqtt`,
+          config?.clientId
+        );    
 
-          client.onConnectionLost = (responseObject) => {
-            if (responseObject.errorCode !== 0) {
-              console.log("Connection lost:", responseObject.errorMessage);
-              setupMqtt()
-            }
-          };
+        client.onConnectionLost = (responseObject) => {
+          if (responseObject.errorCode !== 0) {
+            setupMqtt()
+            //console.log("Connection lost:", responseObject.errorMessage);
+          }
+        };
 
-          client.onMessageArrived = (message) => {
-            const locationData = JSON.parse(message.payloadString);
-              const coordinates = locationData.location.geometry.coordinates;
-            setLocation({ lat:coordinates[1], lng:coordinates[0] });
-          };
+        client.onMessageArrived = (message) => {
+          const locationData = JSON.parse(message.payloadString);
+          const coordinates = locationData.location.geometry.coordinates;
+          console.log(coordinates)
+          setLocation({ lat: coordinates[1], lng: coordinates[0] });
+        };
 
-          client.connect(options);
-        } catch (error) {
-          // Reconnect on fail
-          setupMqtt();
-        }
-      };
-      setupMqtt();
-    
-
+        client.connect(options);
+      } catch (error) {
+        // Reconnect on fail
+        setupMqtt();
+      }
+    };
+    setupMqtt();
   }, [config]);
 
   return location;
